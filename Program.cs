243ede47a7b8af047fc01326a3aa44d8438c37df
Anton Sopov <recorrd@yandex.ru>
2024-02-24 23:24:02 +0300

    Add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите строку из слов разделённых пробелом: ");
+string textInput = Console.ReadLine()!;
+string[] word = textInput.Split(' ');
+string textOut = string.Empty;
+for (int i = 0; i < word.Length; i++)
+{
+    if (word[i].Length <= 3 && i < word.Length)
+    {
+        if (word[i] != "")
+        {
+            textOut += word[i] + " ";
+        }
+    }
+}
+string[] outText = textOut.Split(' ');
+string[] finText = new string[outText.Length - 1];
+for (int j = 0; j < outText.Length - 1; j++)
+{
+    if ((outText[j + 1] == "" || outText[j + 1] == " ") && j == outText.Length - 1)
+    {
+        continue;
+    }
+    finText[j] = outText[j];
+}
+Console.WriteLine(finText.Length);
+Console.WriteLine($"Новая строка: [{string.Join(" ", finText)}]");
